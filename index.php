5fac419c4d05ffa4de56f9668890da532869819b
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-04-07 12:10:55 +0200

    insert data into table

--- index.php
+++ index.php
@@ -56,54 +56,43 @@ $hotels = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
-<body>
-    <!-- <ul>
-        <?php for ($i = 0; $i < count($hotels); $i++) {
-            $hotel = $hotels[$i];
-        ?>
-            <li><?php echo $hotel['name']; ?></li>
-            <li><?php echo $hotel['description']; ?></li>
-            <li><?php echo $hotel['parking'] ? 'Vero' : 'Falso'; ?></li>
-            <li><?php echo $hotel['vote']; ?></li>
-            <li><?php echo $hotel['distance_to_center']; ?></li>
-        <?php } ?>
-    </ul> -->
-
+<body class="p-5">
+    <!-- Hotels Table -->
     <table class="table table-light table-striped table-hover table-bordered">
         <thead class="table-dark">
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Voto</th>
+                <th scope="col">Distanza dal centro città</th>
             </tr>
         </thead>
         <tbody class="table-group-divider">
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
+            <?php for ($i = 0; $i < count($hotels); $i++) {
+                $hotel = $hotels[$i];
+            ?>
+                <tr>
+                    <th scope="row">
+                        <?php echo $hotel['name']; ?>
+                    </th>
+                    <td>
+                        <?php echo $hotel['description']; ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['parking'] ? 'Vero' : 'Falso'; ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['vote']; ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['distance_to_center']; ?>
+                    </td>
+                </tr>
+            <?php } ?>
         </tbody>
     </table>
+    <!-- // Hotels Table -->
 </body>
 
 </html>
